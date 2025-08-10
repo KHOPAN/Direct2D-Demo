20d0bd3d25f309e5a7e4d8db954aa3ac3f7e6f71
KHOPAN <puthipnakchan@gmail.com>
2025-08-11 05:30:36 +0700

    Add window creation boilerplate

--- OpenGL-Demo/main.c
+++ OpenGL-Demo/main.c
@@ -1,39 +1,20 @@
-#define GLEW_STATIC
 #include <stdio.h>
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
+#include <Windows.h>
 
-int main(int argc, char** argv) {
-	if(!glfwInit()) {
+#define CLASS_NAME L"DemoDirect2DClassName"
+
+int WINAPI WinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE previousInstance, _In_ LPSTR command, _In_ int show) {
+	WNDCLASSEXW windowClass = {0};
+	windowClass.cbSize = sizeof(WNDCLASSEXW);
+	windowClass.lpfnWndProc = 0;
+	windowClass.hInstance = instance;
+	windowClass.hCursor = LoadCursorW(NULL, IDC_ARROW);
+	windowClass.lpszClassName = CLASS_NAME;
+
+	if(!RegisterClassExW(&windowClass)) {
 		return 1;
 	}
 
-	GLFWwindow* window = glfwCreateWindow(600, 400, "Hello, world!", NULL, NULL);
-
-	if(!window) {
-		glfwTerminate();
-		return 1;
-	}
-
-	glfwMakeContextCurrent(window);
-
-	if(glewInit() != GLEW_OK) {
-		return 1;
-	}
-
-	printf("OpenGL: %s\n", glGetString(GL_VERSION));
-
-	while(!glfwWindowShouldClose(window)) {
-		glClear(GL_COLOR_BUFFER_BIT);
-		glBegin(GL_TRIANGLES);
-		glVertex2f(-0.5f, -0.5f);
-		glVertex2f(0.0f, 0.5f);
-		glVertex2f(0.5f, -0.5f);
-		glEnd();
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-	}
-
-	glfwTerminate();
+	UnregisterClassW(CLASS_NAME, instance);
 	return 0;
 }
