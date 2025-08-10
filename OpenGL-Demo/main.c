5253cc78fa001368edf792144dc840885d734eb6
KHOPAN <puthipnakchan@gmail.com>
2025-08-11 05:38:10 +0700

    Create a simple window

--- OpenGL-Demo/main.c
+++ OpenGL-Demo/main.c
@@ -3,10 +3,20 @@
 
 #define CLASS_NAME L"DemoDirect2DClassName"
 
+static LRESULT CALLBACK windowProcedure(HWND window, UINT message, WPARAM wparam, LPARAM lparam) {
+	switch(message) {
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	}
+
+	return DefWindowProcW(window, message, wparam, lparam);
+}
+
 int WINAPI WinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE previousInstance, _In_ LPSTR command, _In_ int show) {
 	WNDCLASSEXW windowClass = {0};
 	windowClass.cbSize = sizeof(WNDCLASSEXW);
-	windowClass.lpfnWndProc = 0;
+	windowClass.lpfnWndProc = windowProcedure;
 	windowClass.hInstance = instance;
 	windowClass.hCursor = LoadCursorW(NULL, IDC_ARROW);
 	windowClass.lpszClassName = CLASS_NAME;
@@ -15,6 +25,22 @@ int WINAPI WinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE previousInstance,
 		return 1;
 	}
 
+	HWND window = CreateWindowExW(0L, CLASS_NAME, L"Demo Direct2D", WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, 600, 400, NULL, NULL, instance, NULL);
+	int codeExit = 1;
+
+	if(!window) {
+		goto unregisterClass;
+	}
+
+	MSG message;
+
+	while(GetMessageW(&message, NULL, 0, 0)) {
+		TranslateMessage(&message);
+		DispatchMessageW(&message);
+	}
+
+	codeExit = 0;
+unregisterClass:
 	UnregisterClassW(CLASS_NAME, instance);
-	return 0;
+	return codeExit;
 }
